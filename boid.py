895c4af1b54597baa8af38a8d08615767135e9a1
$!DdH@₹tH V€₹M@ <siddharthverma3904@gmail.com>
2024-03-08 16:44:16 +0530

    added biases and did hyperparameter tuning

--- boid.py
+++ boid.py
@@ -4,13 +4,15 @@ import random
 import matplotlib.animation as animation
 
 # hyperparameters
-vmax = 4
-vmin = 1
-r2 = 8
-avoid = 0.05
+vmax = 5
+vmin = 2
+r2 = 500
+close2 = 2000
+avoid = 0.01
 aligning = 0.05
-cohede = 0.005
-turnfactor = 0.2
+cohede = 0.05
+turnfactor = 0.8
+biasval = 0.0005
 
 # dimensions
 n = 50
@@ -18,7 +20,7 @@ L = -100
 R = 100
 U = 100
 D = -100
-margin = 10
+margin = 20
 dt = 10
 
 # boid class
@@ -30,37 +32,42 @@ class Boid:
         self.vy = vy
         self.b_id = b_id
         self.neighbours = []
+        self.group = random.choice([0, 1, 2])
 
     def move(self):
         # separation
         close_dx = 0
         close_dy = 0
-        for boid in self.neighbours:
-            dx = self.x - boid.x
-            dy = self.y - boid.y
-            if dx**2 + dy**2 < avoid:
-                close_dx += dx
-                close_dy += dy
-        self.vx += close_dx * avoid
-        self.vy += close_dy * avoid
-
         # alignment
         avg_vx = 0
         avg_vy = 0
+        # cohesion
+        avg_x = 0
+        avg_y = 0
         for boid in self.neighbours:
+            dx = self.x - boid.x
+            dy = self.y - boid.y
             avg_vx += boid.vx
             avg_vy += boid.vy
-        if self.neighbours:
-            avg_vx /= len(self.neighbours)
-            avg_vy /= len(self.neighbours)
-            self.vx += (avg_vx - self.vx) * aligning
-            self.vy += (avg_vy - self.vy) * aligning
-
-        # cohesion
-        avg_x = sum(boid.x for boid in self.neighbours) / len(self.neighbours) if self.neighbours else 0
-        avg_y = sum(boid.y for boid in self.neighbours) / len(self.neighbours) if self.neighbours else 0
-        self.vx += (avg_x - self.x) * cohede
-        self.vy += (avg_y - self.y) * cohede
+            avg_x += boid.x
+            avg_y += boid.y
+            if dx**2 + dy**2 < close2:
+                close_dx += dx
+                close_dy += dy
+        if self.neighbours :
+            n = len(self.neighbours)
+            avg_vx = sum(boid.vx for boid in self.neighbours)/n
+            avg_vy = sum(boid.vy for boid in self.neighbours)/n
+            avg_x = sum(boid.x for boid in self.neighbours)/n
+            avg_y = sum(boid.y for boid in self.neighbours)/n
+        else :
+            avg_vx = self.vx
+            avg_vy = self.vy
+            avg_x = self.x
+            avg_y = self.y
+        
+        self.vx += close_dx * avoid + (avg_x - self.x) * cohede + (avg_vx - self.vx) * aligning
+        self.vy += close_dy * avoid + (avg_y - self.y) * cohede + (avg_vy - self.vy) * aligning
 
         # wall avoidance
         if self.x < L+margin:
@@ -71,6 +78,12 @@ class Boid:
             self.vy = self.vy - turnfactor
         if self.y < D+margin:
             self.vy = self.vy + turnfactor
+
+        if self.group == 1 :
+            self.vx = (1 - biasval)*self.vx + biasval
+        # biased to left of screen
+        elif self.group == 2 :
+            self.vx = (1 - biasval)*self.vx -biasval
 
         # speed check
         speed = (self.vx ** 2 + self.vy ** 2) ** 0.5
@@ -112,5 +125,5 @@ def motion(flock):
     plt.show()
 
 # initialize flock
-flock = [Boid(random.uniform(L+margin, R-margin), random.uniform(D+margin, U-margin), random.uniform(0, 2*vmax), random.uniform(vmin, vmax), b_id) for b_id in range(n)]
+flock = [Boid(random.uniform(L+margin, R-margin), random.uniform(D+margin, U-margin), random.uniform(0, 3*vmax), random.uniform(vmin, vmax), b_id) for b_id in range(n)]
 motion(flock)
